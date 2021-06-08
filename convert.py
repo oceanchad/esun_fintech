6020211a4e30f8dce91213d2730555fa0ecc047b
yczhonga <yczhonga@gmail.com>
2021-06-08 21:06:39 +0800

    update convert.py for test data on June. 8th

--- convert.py
+++ convert.py
@@ -2,13 +2,16 @@ import numpy as np
 import cv2
 import base64
 import os
+from collections import defaultdict
+import matplotlib
 """
 This script converts into images test data recorded by server during competition rehearsal 
 It also auto rename file names with image label after it's manually identified and put in labels.txt
 """
 
-FILE_NAME = "temp_data/esun_post_data.json"
-IMAGE_PATH = "temp_data/test_data"
+FILE_NAME = "temp_data/esun_post_data0608.json"
+INTERMEDIATE = "temp_data/esun_post_data0608_formatted.txt"
+IMAGE_PATH = "temp_data/test_data_0608"
 LABEL_NAME = "labels.txt"
 
 def base64_to_binary_for_cv2(image_64_encoded):
@@ -24,19 +27,27 @@ def base64_to_binary_for_cv2(image_64_encoded):
     image = cv2.imdecode(np.frombuffer(img_binary, np.uint8), cv2.IMREAD_COLOR)
     return image
 
+def format(input, output):
+    with open(input) as file:
+        tmp = open(output, '+w')
+        for line in file:
+            line = line.replace(", ", ",\n\t").replace("}", "\n}").replace("{", "{\n\t")
+            tmp.writelines(line)
+
 def code_to_img(input):
     """
     input is a collection of object in json format recorded by server
     """
-    with open(input) as file:
-        words = set()
+    with open(INTERMEDIATE) as file:
+        words = defaultdict(lambda: 0)
         for line in file:
             l = line.strip()
             if l.startswith("\"image\""):
-                words.add(l.split(':')[1].strip('" '))
+                words[l.split(':')[1].strip('" ')] += 1
 
     os.chdir(IMAGE_PATH)
-    for i, word in enumerate(words):
+
+    for i, word in enumerate(words.keys()):
         image = base64_to_binary_for_cv2(word)
         file_name = str(i) + '.jpg'
         cv2.imwrite(file_name, image)
@@ -68,7 +79,8 @@ def label_img(label_file):
                     index += 1
 
 def main():
-    code_to_img(FILE_NAME)
+    format(FILE_NAME, INTERMEDIATE)
+    code_to_img(INTERMEDIATE)
     # label_img(LABEL_NAME) # call this function after image labels are manully identified and put in labels.txt
 
 if __name__ == "__main__":
